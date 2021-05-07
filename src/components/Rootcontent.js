9313e1d0d04761ff6a6deafd1ada3fa3d1a32f6b
SeongBeom27 <lgh121546@naver.com>
2021-05-07 18:18:03 +0900

    Youtube Clone Start

---
+++ src/components/Rootcontent.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class Rootcontent extends Component {
+  render() {
+    return (
+      <div className="content">
+        <div className="cont1">
+          <div className="ad">AD</div>
+          <div className="adplus">ADplus</div>
+        </div>
+        <div className="title1">title1</div>
+        <div className="cont2">
+          <div className="v1_1">영상1</div>
+          <div className="v1_2">영상2</div>
+          <div className="v1_3">영상3</div>
+          <div className="v1_4">영상4</div>
+          <div className="v1_5">영상5</div>
+          <div className="v1_6">영상6</div>
+        </div>
+        <div className="title2">title2</div>
+        <div className="cont3">
+          <div className="v2_1">영상1</div>
+          <div className="v2_2">영상2</div>
+          <div className="v2_3">영상3</div>
+          <div className="v2_4">영상4</div>
+          <div className="v2_5">영상5</div>
+          <div className="v2_6">영상6</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Rootcontent
